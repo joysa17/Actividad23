7e6d3d07dde35af507dbdccf78a9d7625a864e47
joysa <joselin.solis.3b@gmail.com>
2017-10-20 22:31:56 -0300

    cuarto commit

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,14 +8,10 @@ Rails.application.config.assets.version = '1.0'
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 Rails.application.config.assets.paths << Rails.root.join('vendor', 'css')
-Rails.application.config.assets.paths << Rails.root.join('vendor', 'js')
 Rails.application.config.assets.paths << Rails.root.join('vendor', 'fonts')
 Rails.application.config.assets.paths << Rails.root.join('vendor', 'img')
-Rails.application.config.assets.paths << Rails.root.join('vendor', 'img','backgrounds')
-Rails.application.config.assets.paths << Rails.root.join('vendor', 'img','modal')
-Rails.application.config.assets.paths << Rails.root.join('vendor', 'img','screenshots')
-Rails.application.config.assets.paths << Rails.root.join('vendor', 'img','team')
-Rails.application.config.assets.paths << Rails.root.join('vendor', 'img','testimonials')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'js')
+
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
